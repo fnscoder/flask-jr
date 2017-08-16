afe1a4e61b00b1766f4823e03521057cc22d97a1
fnscoder <felipensouza@live.com>
2017-08-16 18:21:45 -0300

    add templates

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,6 +1,12 @@
+from flask import render_template
 from app import app
 
 
 @app.route("/")
 def index():
-    return "Hello, world."
+    return render_template('index.html')
+
+
+@app.route("/login")
+def login():
+    return render_template('base.html')
